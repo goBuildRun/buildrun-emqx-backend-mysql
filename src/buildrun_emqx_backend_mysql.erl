46306757ee48093bc379142035e801a86632928e
GitHub <arone.lijun@gmail.com>
2020-02-08 15:29:19 +0800

    backend_mysq.erl

--- src/buildrun_emqx_backend_mysql.erl
+++ src/buildrun_emqx_backend_mysql.erl
@@ -76,10 +76,10 @@ load(Env) ->
 %% Client Lifecircle Hooks
 %%--------------------------------------------------------------------
 
-on_client_connected(ClientInfo = #{clientid := ClientId, peerhost := {Peerhost, _}}, ConnInfo, _Env) ->
+on_client_connected(ClientInfo = #{clientid := ClientId, peername := {Peerhost, _}}, ConnInfo, _Env) ->
     emqx_metrics:inc('buildrun.backend.mysql.client_connected'),
     %%buildrun_emqx_backend_mysql_cli:query(?CLIENT_CONNECTED_SQL, [binary_to_list(ClientId),"online",iolist_to_binary(ntoa(Peerhost)),iolist_to_binary(ntoa(Peerhost))]),
-    io:format("Client(~s) connected, ClientInfo:~n~p~n, ConnInfo:~n~p~n", [ClientId, ClientInfo, ConnInfo]).
+    io:format("Client(~s) connected, ClientInfo:~n~p~n, ConnInfo:~n~p~n, Peerhost:~n~p~n", [ClientId, ClientInfo, ConnInfo, Peerhost]).
 
 on_client_disconnected(ClientInfo = #{clientid := ClientId}, ReasonCode, ConnInfo, _Env) ->
     emqx_metrics:inc('buildrun.backend.mysql.client_disconnected'),
