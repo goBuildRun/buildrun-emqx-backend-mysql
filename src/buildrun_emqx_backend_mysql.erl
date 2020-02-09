be67c1d9b66045f62e9a39494fcf8325765b7823
GitHub <arone.lijun@gmail.com>
2020-02-09 11:42:23 +0800

    Qos

--- src/buildrun_emqx_backend_mysql.erl
+++ src/buildrun_emqx_backend_mysql.erl
@@ -106,10 +106,10 @@ on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
 
 on_message_publish(#message{flags = #{retain := true}} = Message, _Env) ->
     #message{id = Id, from = From, topic = Topic, qos = Qos, payload = Payload } = Message,
-    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_RETAIN_SQL, [binary_to_list(Topic),emqx_guid:to_hexstr(Id),binary_to_list(From),binary_to_list(Qos),null,null,null,
+    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_RETAIN_SQL, [binary_to_list(Topic),emqx_guid:to_hexstr(Id),binary_to_list(From),null,null,null,null,
                                           emqx_guid:to_hexstr(Id),binary_to_list(From),null,null,null]),
     %%buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),binary_to_list(Topic),null,null,binary_to_list(Payload),timestamp()]),
-    io:format("Publish ~s~n", [emqx_message:format(Message)]),
+    io:format("Qos ~s~n Publish ~s~n", [Qos,emqx_message:format(Message)]),
     {ok, Message}.
 
 
