5c98a3aea8c64cd89d2861008d9097a188c0181e
GitHub <arone.lijun@gmail.com>
2020-02-09 14:27:34 +0800

    modified publish SQL

--- mqtt_msg.sql
+++ mqtt_msg.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `mqtt_msg`;
 CREATE TABLE `mqtt_msg` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `msgid` varchar(100) DEFAULT NULL,
-  `topic` varchar(1024) NOT NULL,
+  `topic` varchar(1024) DEFAULT NULL,
   `sender` varchar(1024) DEFAULT NULL,
   `node` varchar(60) DEFAULT NULL,
   `qos` int(11) NOT NULL DEFAULT '0',
