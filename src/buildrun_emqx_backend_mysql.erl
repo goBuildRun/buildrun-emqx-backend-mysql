a1ab0ba39e5561f8a5b933e962181d8f13a0b03e
GitHub <arone.lijun@gmail.com>
2020-02-09 21:12:17 +0800

    add retain

--- src/buildrun_emqx_backend_mysql.erl
+++ src/buildrun_emqx_backend_mysql.erl
@@ -95,7 +95,7 @@ on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
 
 on_message_publish(#message{flags = #{retain := true}} = Message, _Env) ->
     #message{id = Id, from = From, topic = Topic, qos = Qos, flags = Retain, payload = Payload } = Message,
-    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),Topic,integer_to_list(Qos),null,utf8_binary_to_list(Payload),null]),
+    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),Topic,integer_to_list(Qos),null,binary_to_list(Payload),null]),
     %%io:format("on_message_publish ~s~n", [emqx_message:format(Message)]),
     {ok, Message};
 
