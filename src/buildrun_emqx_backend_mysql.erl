b0de0c04656b6222b5773923f77a0f5eac16699c
GitHub <arone.lijun@gmail.com>
2020-02-09 13:44:54 +0800

    ommit datetime

--- src/buildrun_emqx_backend_mysql.erl
+++ src/buildrun_emqx_backend_mysql.erl
@@ -32,7 +32,7 @@
 -define(MESSAGE_PUBLISH_SQL,
                  <<"insert into mqtt_msg(msgid, sender, "
                    "topic, qos, retain, payload, arrived) "
-                   "values (?, ?, ?, ?, ?, ?, now())">>).
+                   "values (?, ?, ?, ?, ?, ?, ? ">>).
 
 -define(MESSAGE_RETAIN_SQL,
                  <<"insert into mqtt_retain(topic, msgid, "
@@ -104,7 +104,7 @@ on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
 
 on_message_publish(#message{flags = #{retain := true}} = Message, _Env) ->
     #message{id = Id, from = From, topic = Topic, qos = Qos, payload = Payload } = Message,
-    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),binary_to_list(Topic),null,null,null]),
+    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),binary_to_list(Topic),null,null,null,null]),
     %%buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),binary_to_list(Topic),null,null,binary_to_list(Payload),timestamp()]),
     io:format("Qos Publish ~s~n", [emqx_message:format(Message)]),
     {ok, Message}.
