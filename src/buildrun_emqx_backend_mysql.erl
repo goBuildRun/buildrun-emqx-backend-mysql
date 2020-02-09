43cd9c6078fd8bc14078cac3897e45759c4c9169
GitHub <arone.lijun@gmail.com>
2020-02-09 10:19:58 +0800

    backend_mysq.erl

--- src/buildrun_emqx_backend_mysql.erl
+++ src/buildrun_emqx_backend_mysql.erl
@@ -105,8 +105,8 @@ on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
     {ok, Message};
 
 on_message_publish(#message{flags = #{retain := true}} = Message, _Env) ->
-    #message{id = Id, from = From, topic = Topic, qos = Qos, flags = Retain, payload = Payload } = Message,
-    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),binary_to_list(Topic),null,null,null]),
+    #message{id = Id, from = From, topic = Topic, qos = Qos, payload = Payload } = Message,
+    buildrun_emqx_backend_mysql_cli:query(?MESSAGE_RETAIN_SQL, [binary_to_list(Topic),emqx_guid:to_hexstr(Id),From,null,null,null]),
     %%buildrun_emqx_backend_mysql_cli:query(?MESSAGE_PUBLISH_SQL, [emqx_guid:to_hexstr(Id),binary_to_list(From),binary_to_list(Topic),null,null,binary_to_list(Payload),timestamp()]),
     io:format("Publish ~s~n", [emqx_message:format(Message)]),
     {ok, Message}.
